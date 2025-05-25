c0ae0de2000469cb3f3d15e5cf380886e133f946
habeeb-code-cell <mustaphahabeebullah2003@gmail.com>
2025-05-25 23:52:37 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';   // You can create this file or remove this import if you don’t have it
 import App from './App';
 
+
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
